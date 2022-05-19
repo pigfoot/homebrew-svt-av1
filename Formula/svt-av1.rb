c27947723d302f86745ea4f416f9ba60c5df102e
George Carlson <74246849+georgemcarlson@users.noreply.github.com>
2022-05-19 00:18:11 -0500

    Update svt-av1.rb

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -4,8 +4,7 @@
 class SvtAv1 < Formula
   desc "SVT-AV1 video codec"
   homepage "https://gitlab.com/AOMediaCodec/SVT-AV1"
-  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v0.9.0/SVT-AV1-v0.9.0.tar.gz"
-  sha256 "51a41fc38ced0e9a56aa4300b1d872f3b43e2421644110852dfdb67440080c63"
+  url "https://gitlab.com/AOMediaCodec/SVT-AV1/-/archive/v1.1.0/SVT-AV1-v1.1.0.tar.gz"
   head "https://gitlab.com/AOMediaCodec/SVT-AV1"
   license "BSD-3-Clause"
 
